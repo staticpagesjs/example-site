6fa4344d765b11b7bf2277efe77f0fe39b649b11
root <root@localhost>
2022-07-11 22:34:05 +0200

    add minor comment

--- generate.js
+++ generate.js
@@ -75,6 +75,8 @@ staticPages({
 		// replace filename (last item) with title
 		d.breadcrumbs.splice(-1, 1, d.title || d.header.basename);
 
+		// when returning undefined the content is not rendered.
+		// it is possible to return an array, in that case multiple pages are rendered.
 		return d;
 	},
 	to: twigWriter({

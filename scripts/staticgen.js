6a09e60aaee6d8b9fe3a73b4e85595aa2a9b193a
root <root@localhost>
2024-01-21 03:16:14 +0100

    Simplify

--- scripts/staticgen.js
+++ scripts/staticgen.js
@@ -1,25 +1,19 @@
-const { staticPages } = require('@static-pages/core');
-const { twig, raw } = require('@static-pages/twig');
-
-const fs = require('node:fs');
-const path = require('node:path');
-const YAML = require('yaml');
+import { staticPages, read } from '@static-pages/starter/node';
+import { twig, raw } from '@static-pages/twig';
+import { posix as posixPath } from 'node:path';
+import { stringify } from 'yaml';
 
 const startTime = new Date();
 
-// this will read language specific values
+// translations (i18n) bags
 const messages = {};
-const bags = fs.readdirSync('messages', { recursive: false, withFileTypes: false, encoding: 'utf8' });
-for (const bag of bags) {
-	const extname = path.extname(bag);
-	const bagPath = path.join('messages', bag);
-	const stat = fs.statSync(bagPath);
-	if (stat.isFile() && extname === '.yaml') {
-		const lang = path.basename(bag, extname);
-		messages[lang] = YAML.parse(fs.readFileSync(bagPath, 'utf-8'));
-	}
+for await (const bag of read({ cwd: 'messages' })) {
+	// .url inherits the filename if property not present
+	messages[bag.url] = bag;
+	delete bag.url;
 }
 
+// json/yaml stringify replacer to be compatible with twig context
 const replacer = (key, value) => {
 	if (!Array.isArray(value) && typeof value?.entries === 'function') {
 		return Object.fromEntries(value.entries());
@@ -55,13 +49,13 @@ staticPages({
 					return raw(JSON.stringify(subject, replacer, '\t'));
 				},
 				yaml(_context, subject) {
-					return raw(YAML.stringify(subject, replacer, { lineWidth: Number.POSITIVE_INFINITY }));
+					return raw(stringify(subject, replacer, { lineWidth: Number.POSITIVE_INFINITY }));
 				},
 			},
 			functions: {
 				relative({ context }, url) {
 					if (url.startsWith('/')) {
-						const { join, relative, dirname, basename } = path.posix;
+						const { join, relative, dirname, basename } = posixPath;
 						const selfUrl = join('/', context.get('url'));
 						const relativeBase = relative(dirname(selfUrl), dirname(url));
 						return join(relativeBase, basename(url));
